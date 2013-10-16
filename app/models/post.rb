5470cb083e43bf168fdce2fcaf002e5ebaac9cb1
Zoe <zoedeans@gmail.com>
2013-10-16 10:54:09 +0000

    fixed the 5 letter name problem

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,5 @@
 class Post < ActiveRecord::Base
 	attr_accessible :text, :title, :post_id
-  	validates :title, presence: true,
-                    length: { minimum: 5 }
+  	validates :title, presence: true
 
 end
